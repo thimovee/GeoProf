5288951e2c1126ef3990ce3fb27b6ed986a375cf
thimovee <thimove@gmail.com>
2022-12-16 10:28:10 +0100

    Edited create

--- ProjectGeoProfs/Models/VerlofAanvraag.cs
+++ ProjectGeoProfs/Models/VerlofAanvraag.cs
@@ -6,16 +6,21 @@ namespace ProjectGeoProfs.Models
     {
         [Display(Name = "ID")]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Vul je volledige naam in.")]
         [Display(Name = "Volledige Naam")]
         public string? VolledigeNaam { get; set; }
+        [Required(ErrorMessage = "Vul de sector in waar je werkt.")]
         public string? Sector { get; set; }
+        [Required(ErrorMessage = "Vul de startdatum van je verlof in")]
         [Display(Name = "Startdatum Verlof")]
         [DataType(DataType.Date)]
         public DateTime VerlofBeginDatum { get; set; }
+        [Required(ErrorMessage = "Vul de startdatum van je verlof in")]
         [Display(Name = "Einddatum Verlof")]
         [DataType(DataType.Date)]
         public DateTime VerlofEindDatum { get; set; }
         [Display(Name = "Verlof Reden")]
+        [Required(ErrorMessage = "Vul je Verlof reden in.")]
         public string? VerlofReden { get; set; }
         public bool Geaccepteerd { get; set; }
     }
